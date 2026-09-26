73b0ab2918ed32398b280c7d3ef724f1a3cb0b09
smitsmitthakkar7-rgb <smitsmitthakkar7@gmail.com>
2026-09-26 08:41:01 +0530

    Add files via upload

---
+++ session 2.py
@@ -0,0 +1,45 @@
+
+# 1
+
+"""
+student_name="smit"
+fav_app="free fire"
+screentime_limit=3
+
+print(student_name)
+print(fav_app)
+print(screentime_limit)
+"""
+
+# 2 
+"""
+product_name="iphone"
+price=50000
+its_avalable=True
+
+print(product_name)
+print(price)
+print(its_avalable)
+"""
+
+"""order_total=500
+delivery_region="ahmedabad"
+discount_percentage=10
+
+print(order_total)
+print(delivery_region)
+print(discount_percentage)
+"""
+
+# 5 
+# wrong 
+
+"""if True:
+print("hello")
+"""
+
+# right 
+
+"""if True:
+    print("Hello")
+"""
